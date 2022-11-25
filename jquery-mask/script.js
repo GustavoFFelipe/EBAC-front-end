55736a2df2f3d089efbb508f6bc12e629b1d202b
Gustavo Felipe <gustavo.ffelipedev@gmail.com>
2022-11-25 13:50:34 -0300

    mask jquery

---
+++ jquery-mask/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $('#phone').mask('(00) 0 0000-0000', {
+        placeholder: '(00) 0 0000-0000'
+    });
+    $('#cpf').mask('000.000.000-00', {
+        placeholder: '000.000.000-00'
+    });
+    $('#cep').mask('00.000-000', {
+        placeholder: '00.000-00'
+    });
+
+})
