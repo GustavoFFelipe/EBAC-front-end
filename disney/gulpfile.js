508d2cc16ad6d9f29427207d8aa7335c8ef5ef5f
Gustavo Felipe <gustavo.ffelipedev@gmail.com>
2022-12-08 20:12:56 -0300

    hero contant

--- disney/gulpfile.js
+++ disney/gulpfile.js
@@ -1,14 +1,23 @@
+const { parallel } = require('gulp');
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
-
+/**compriminco styles  */
 function styles(){
     return gulp.src('./src/styles/*.scss')
     .pipe(sass({outputStyle: 'compressed'}))
     .pipe(gulp.dest('./dist/css'))
 }
 
-exports.default = styles;
+/* comprimindo images*/
+function images(){
+    return gulp.src('./src/images/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/images'))
+}
+
+exports.default = gulp.parallel(styles, images);
 exports.watch = function(){
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
 }
