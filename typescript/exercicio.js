d2dff4780e54ce23c13095ef034e6186e27c9c05
Gustavo Felipe <gustavo.ffelipedev@gmail.com>
2023-03-04 13:37:07 -0300

    exercicio_ts

---
+++ typescript/exercicio.js
@@ -0,0 +1,5 @@
+"use strict";
+const caluloAreaCampo = (largura, comprimento) => console.log(largura * comprimento);
+const welcome = (nome) => console.log(`Olá ${nome}`);
+caluloAreaCampo(20, 10);
+welcome("Gustavo");
---
+++ typescript/exercicio.ts
@@ -0,0 +1,7 @@
+
+
+const caluloAreaCampo = (largura: number, comprimento: number) => console.log(largura * comprimento);
+
+const welcome = (nome: string) => console.log(`Olá ${nome}`);
+caluloAreaCampo(20, 10);
+welcome("Gustavo") ;
